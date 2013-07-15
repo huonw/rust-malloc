0a841e5379c5c68f28d036794eab0a2ac3afad1c
Huon Wilson <dbau.pp+github@gmail.com>
2013-07-15 11:47:12 +1000

    Update syscall to be correct.

--- syscall.rs
+++ syscall.rs
@@ -4,7 +4,7 @@
 pub unsafe fn syscall0(n: int) -> int {
     let mut ret : int = 0;
 
-    asm!("syscall" : "=ra"(ret) : "ra"(n) : "rcx", "r11", "memory" : "volatile");
+    asm!("syscall" : "={rax}"(ret) : "{rax}"(n) : "rcx", "r11", "memory" : "volatile");
 
     return ret;
 }
@@ -13,15 +13,7 @@ pub unsafe fn syscall0(n: int) -> int {
 pub unsafe fn syscall1(n: int, a1: int) -> int {
     let mut ret : int = 0;
 
-    asm!(
-        "
-movq $1, %rax
-movq $2, %rdi
-syscall
-movq %rax, $0
-": "=r"(ret): "r"(n), "r"(a1) : "rax", "rcx", "rdi", "r11", "memory" : "volatile"
-    );
-    //asm!("syscall" : "=ra"(ret) : "ra"(n), "rD"(a1) : "rcx", "r11", "memory" : "volatile");
+    asm!("syscall" : "={rax}"(ret) : "{rax}"(n), "{rdi}"(a1) : "rcx", "r11", "memory" : "volatile");
 
     return ret;
 }
@@ -30,7 +22,7 @@ movq %rax, $0
 pub unsafe fn syscall2(n: int, a1: int, a2: int) -> int {
     let mut ret : int = 0;
 
-    asm!("syscall" : "=ra"(ret) : "0"(n), "rS"(a1), "rD"(a2) : "rcx", "r11", "memory" : "volatile");
+    asm!("syscall" : "={rax}"(ret) : "{rax}"(n), "{rdi}"(a1), "{rsi}"(a2) : "rcx", "r11", "memory" : "volatile");
 
     return ret;
 }
@@ -40,7 +32,7 @@ pub unsafe fn syscall3(n: int, a1: int, a2: int, a3: int) -> int {
 
     let mut ret : int = 0;
 
-    asm!("syscall" : "=ra"(ret) : "ra"(n), "rD"(a1), "rS"(a2), "rd"(a3) : "rcx", "r11", "memory" : "volatile");
+    asm!("syscall" : "={rax}"(ret) : "{rax}"(n), "{rdi}"(a1), "{rsi}"(a2), "{rdx}"(a3) : "rcx", "r11", "memory" : "volatile");
 
     return ret;
 }
